8762c7440e43eb76c33b8b61faea3dc741b99097
kashyap-29 <kashyap.vs303@gmail.com>
2020-09-10 19:36:18 -0500

    code added

--- src/main/scala/com/mahipal/org/Sample.scala
+++ src/main/scala/com/mahipal/org/Sample.scala
@@ -1,5 +1,16 @@
 package com.mahipal.org
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 object Sample {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setMaster("local").setAppName("DemoOne")
+    val sc = new SparkContext(conf)
+
+
+
+  }
 
 }
