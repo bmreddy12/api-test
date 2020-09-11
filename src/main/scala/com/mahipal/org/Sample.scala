6b80f5b49b2b686b1d8a1ca76e8ce720a463fe73
kashyap-29 <kashyap.vs303@gmail.com>
2020-09-10 20:03:13 -0500

    code added

--- src/main/scala/com/mahipal/org/Sample.scala
+++ src/main/scala/com/mahipal/org/Sample.scala
@@ -1,13 +1,16 @@
 package com.mahipal.org
 
 import org.apache.spark.{SparkConf, SparkContext}
-
+import java.util.Collections
 object Sample {
 
   def main(args: Array[String]): Unit = {
 
     val conf = new SparkConf().setMaster("local").setAppName("DemoOne")
     val sc = new SparkContext(conf)
+    //val lii = (1,2,3)
+    val li = sc.parallelize(Seq(1,2,5,3))
+    println(li.count())
 
 
 
